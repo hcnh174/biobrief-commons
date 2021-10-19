93e764ea7d6f2cdbd8c5cc5ca77ebac5e0c13dcc
hcnh174 <nelsonhayes4@gmail.com>
2021-10-19 14:44:41 +0900

    added optional message before and after running Thread.sleep

--- biobrief/src/main/java/org/biobrief/util/ThreadHelper.java
+++ biobrief/src/main/java/org/biobrief/util/ThreadHelper.java
@@ -3,6 +3,8 @@ package org.biobrief.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Stopwatch;
+
 public final class ThreadHelper
 {	
 	@SuppressWarnings("unused")	private static final Logger log=LoggerFactory.getLogger(ThreadHelper.class);
@@ -22,5 +24,13 @@ public final class ThreadHelper
 			StringHelper.println("sleep interrupted: "+e);
 		}
 		//logger.debug("waking up after "+stopwatch.stop()+" milliseconds");
+	}
+	
+	public static void sleep(long millis, MessageWriter out)
+	{
+		out.println("sleeping for "+millis+" milliseconds");
+		Stopwatch stopwatch=Stopwatch.createStarted();
+		sleep(millis);
+		out.println("waking up after "+stopwatch.stop()+" milliseconds");
 	}
 }
