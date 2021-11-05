983d4b4cd82eac82c9138ff980818e8a383e5131
hcnh174 <nelsonhayes4@gmail.com>
2021-11-05 12:39:19 +0900

    apache poi 5.1 broke Docx4j, so switching to poi word extractor

--- biobrief/src/main/java/org/biobrief/util/WordHelper.java
+++ biobrief/src/main/java/org/biobrief/util/WordHelper.java
@@ -1,16 +1,24 @@
 package org.biobrief.util;
 
 import java.io.File;
+import java.io.InputStream;
 
 import org.apache.poi.hssf.record.crypto.Biff8EncryptionKey;
-import org.apache.poi.hwpf.extractor.WordExtractor;
-//import org.apache.poi.xwpf.extractor.XPFFWordExtractor;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.poifs.crypt.Decryptor;
+import org.apache.poi.poifs.crypt.EncryptionInfo;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+//import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
 
 //https://www.tutorialspoint.com/apache_poi_word/apache_poi_word_quick_guide.htm
 //http://poi.apache.org/text-extraction.html
 //http://poi.apache.org/apidocs/4.1/org/apache/poi/hwpf/extractor/WordExtractor.html#WordExtractor-org.apache.poi.poifs.filesystem.POIFSFileSystem-
+//https://poi.apache.org/text-extraction.html
 //https://poi.apache.org/encryption.html
+//http://useof.org/java-open-source/org.apache.poi.xwpf.usermodel.XWPFDocument/9
 public class WordHelper
 {	
 	public static final String DOCX="docx";
@@ -20,14 +28,56 @@ public class WordHelper
 		try
 		{
 			FileHelper.checkExists(filename);
-			Biff8EncryptionKey.setCurrentUserPassword(password);
-			POIFSFileSystem fs = new POIFSFileSystem(new File(filename), true);
-			WordExtractor extractor = new WordExtractor(fs);
-			return extractor.getText();
+//			File file=new File(filename);
+//			NPOIFSFileSystem filesystem = new NPOIFSFileSystem(file, true);
+//			EncryptionInfo info = new EncryptionInfo(filesystem);
+//			Decryptor d = Decryptor.getInstance(info);
+//			d.verifyPassword(password);
+//			InputStream dataStream = d.getDataStream(filesystem);
+//			OPCPackage opc = OPCPackage.open(dataStream);
+//			XWPFDocument doc = new XWPFDocument(opc);
+//			XWPFWordExtractor ex = new XWPFWordExtractor(doc);
+//			String text = ex.getText();
+//			return text;
+			
+			File file=new File(filename);
+			POIFSFileSystem filesystem = new POIFSFileSystem(file, true);
+			EncryptionInfo info = new EncryptionInfo(filesystem);
+			Decryptor d = Decryptor.getInstance(info);
+			d.verifyPassword(password);
+			InputStream dataStream = d.getDataStream(filesystem);
+			OPCPackage opc = OPCPackage.open(dataStream);
+			XWPFDocument doc = new XWPFDocument(opc);
+			XWPFWordExtractor ex = new XWPFWordExtractor(doc);
+			String text = ex.getText();
+			return text;
+			
+//			Biff8EncryptionKey.setCurrentUserPassword(password);
+//			POIFSFileSystem fs = new POIFSFileSystem(new File(filename), true);
+////			//WordExtractor extractor = new WordExtractor(fs);
+//			XWPFWordExtractor extractor=new XWPFWordExtractor(fs);
+//			return extractor.getText();
 		}
 		catch (Exception e)
 		{
 			throw new CException(e);
 		}		
 	}
+	
+//	public static String extractText(String filename, String password)
+//	{
+//		try
+//		{
+//			FileHelper.checkExists(filename);
+//			Biff8EncryptionKey.setCurrentUserPassword(password);
+//			POIFSFileSystem fs = new POIFSFileSystem(new File(filename), true);
+//			//WordExtractor extractor = new WordExtractor(fs);
+//			XWPFWordExtractor extractor=new XWPFWordExtractor(fs);
+//			return extractor.getText();
+//		}
+//		catch (Exception e)
+//		{
+//			throw new CException(e);
+//		}		
+//	}
 }
--- biobrief/src/test/java/org/biobrief/util/TestWordHelper.java
+++ biobrief/src/test/java/org/biobrief/util/TestWordHelper.java
@@ -5,19 +5,20 @@ import org.junit.jupiter.api.Test;
 //gradle test --stacktrace --info --tests *TestWordHelper
 public class TestWordHelper
 {
-	//@Test
+	@Test
 	public void loadFile()
 	{
-		String filename="c:\\temp\\word.docx";
-		String password="secret";
+		//String filename="c:\\temp\\word.docx";
+		//String password="secret";
+		String filename="x:\\B105602374891\\Seq_B105602374891.docx";
+		String password="B1056";
 		
 		String prefix=FileHelper.getRoot(filename);
 		String outdir=Constants.BIOBRIEF_DIR+"/.temp/word";
 		FileHelper.createDirectory(outdir);
 		
-		StringBuilder buffer=new StringBuilder();
 		String text=WordHelper.extractText(filename, password);
 
-		FileHelper.writeFile(outdir+"/"+prefix+".xml", buffer.toString());
+		FileHelper.writeFile(outdir+"/"+prefix+".xml", text);
 	}
 }
