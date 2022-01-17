63b2f59bc1f3af58662fddb61f73969e4ccaae91
hcnh174 <nelsonhayes4@gmail.com>
2022-01-17 09:43:14 +0900

    updated poi

--- biobrief/src/main/java/org/biobrief/util/PhantomjsHelper.java
+++ biobrief/src/main/java/org/biobrief/util/PhantomjsHelper.java
@@ -1,7 +1,6 @@
 package org.biobrief.util;
 
-//https://www.xpdfreader.com/pdftotext-man.html
-//pdftotext -enc UTF-8 -upw password -layout /mnt/work/pdf/c-cat.pdf /mnt/work/pdf/c-cat.txt
+//run_phantomjs.sh -htmlfile /mnt/out/phantomjs/test.html --pdffile /mnt/out/phantomjs/test.pdf
 public class PhantomjsHelper
 {
 	public static int convertHtmlToPdf(String htmlfile, String pdffile)
@@ -9,11 +8,6 @@ public class PhantomjsHelper
 		System.out.println("htmlfile="+htmlfile);
 		FileHelper.checkExists(htmlfile);
 		String command=Constants.SCRIPTS_DIR+"/run_phantomjs.sh --htmlfile "+htmlfile+" --pdffile "+pdffile;
-		return execute(command, pdffile);
-	}
-	
-	private static int execute(String command, String pdffile)
-	{
 		System.out.println("command: "+command);
 		int exitcode=CCommandLine.execute(command);
 		if (exitcode!=0)
