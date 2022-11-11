1c48f161a226faab2189259892f18b1f34ca6383
hcnh174 <nelsonhayes4@gmail.com>
2022-11-11 22:12:10 +0900

    added caching to synology authentication service

--- biobrief/src/main/java/org/biobrief/synology/SynologyAuthenticationService.java
+++ biobrief/src/main/java/org/biobrief/synology/SynologyAuthenticationService.java
@@ -3,6 +3,7 @@ package org.biobrief.synology;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 import org.biobrief.synology.SynologyHelper.SynouserGetCommand;
 import org.biobrief.util.JsonHelper;
@@ -16,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
+import com.google.api.client.util.Sets;
 import com.google.common.collect.Lists;
 
 // https://www.baeldung.com/spring-security-authentication-provider
@@ -23,25 +25,24 @@ import com.google.common.collect.Lists;
 public class SynologyAuthenticationService implements UserDetailsService
 {
 	@Autowired protected SynologyService synologyService;
+	protected Set<String> logins=Sets.newHashSet();
 	protected MessageWriter out=new MessageWriter();//new PrintWriter(System.out));
 	protected String admin_username=RuntimeHelper.getEnvironmentVariable("ADMIN_USERNAME", false);
 	protected String admin_password=RuntimeHelper.getEnvironmentVariable("ADMIN_PASSWORD", false);
-	
-//	public enum Role
-//	{
-//		ROLE_NONE,
-//		ROLE_ADMIN,
-//		ROLE_REPORT_CHECK,
-//		ROLE_CORE_MEMBER;
-//	}
 	
 	public boolean authenticate(String username, String password)
 	{
 		if (username.equals(admin_username) && password.equals(admin_password))
 			return true;
-		
+		String key=username+":"+password;
+		if (logins.contains(key))
+		{
+			System.out.println("SynologyAuthenticationService.authenticate found login in cache username: "+username);
+			return true;
+		}
 		boolean success=synologyService.login(username, password, out);
 		System.out.println("SynologyAuthenticationProvider.login success: "+success);
+		logins.add(key);
 		return success;
 	}
 	
@@ -54,16 +55,6 @@ public class SynologyAuthenticationService implements UserDetailsService
 		
 		SynouserGetCommand.Result result=synologyService.getUser(username, out);
 		System.out.println("SynologyAuthenticationProvider.loadUserByUsername result: "+JsonHelper.toJson(result));
-		
-		
-//		List<Role> roles=Lists.newArrayList();
-//		if (result.getGroups().contains("administrators"))
-//			roles.add(Role.ROLE_ADMIN);
-//		if (result.getGroups().contains("coremembers"))
-//			roles.add(Role.ROLE_CORE_MEMBER);
-//		if (result.getGroups().contains("expertpanel"))
-//			roles.add(Role.ROLE_REPORT_CHECK);
-		
 		List<GrantedAuthority> authorities=getAuthorities(result.getGroups());
 		User user=new User(username, username, authorities);
 
@@ -75,8 +66,6 @@ public class SynologyAuthenticationService implements UserDetailsService
 	{
 		List<GrantedAuthority> authorities=getAuthorities(Lists.newArrayList("administrators"));//Role.ROLE_ADMIN
 		User user=new User(admin_username, admin_username, authorities);
-		//user.setName(admin_username);
-		//user.setAdministrators(true);
 		return user;
 	}
 	
