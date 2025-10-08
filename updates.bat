0e52c2d2139e0f75c96062a4737d6d6424392a67
hcnh174 <nelsonhayes4@gmail.com>
2025-10-08 11:59:11 +0900

    updated dependencies

--- updates.bat
+++ updates.bat
@@ -1,4 +1,4 @@
 TITLE updates
 echo Dependency updates > .temp/updates.txt
-gradle -q dependencyUpdates >> .temp/updates.txt && ^
+gradle -q dependencyUpdates --no-parallel >> .temp/updates.txt && ^
 cd .temp && type updates.txt && cd ..
